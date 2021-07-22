2d2c051eed3a9a2dac1a2530f82188dd03404d46
John D Pell <John+git@gaelicWizard.net>
2021-07-21 22:00:59 -0700

    Add Klingon unicode fonts

---
+++ Casks/pIqaD.rb
@@ -0,0 +1,13 @@
+cask "piqad" do
+  version :latest
+  sha256 "f4ab6a06906d5a0c0913ee611967a1013a104f185051a330e49437db6236c63e"
+
+  url "https://hol.kag.org/pIqaDFontsNormalAndWeb.zip"
+  name "pIqaD"
+  desc "Klingon Unicode Font Package"
+  homepage "https://hol.kag.org/page/piqadsupport.html"
+
+  font "HaSta.ttf"
+  font "kapiqad.otf"
+  font "pIqaD.otf"
+end
